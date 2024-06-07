9df9dd6eb1a16f5977b7ad930fc6a6d6fddba03e
Zhnec163 <studio.dreamlimit@gmail.com>
2024-06-07 18:24:48 +0700

    refactoring

--- Assets/Scripts/TextChanger.cs
+++ Assets/Scripts/TextChanger.cs
@@ -1,19 +1,17 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TextChanger : MonoBehaviour
 {
-    [SerializeField] private Text _text1;
-    [SerializeField] private Text _text2;
-    [SerializeField] private Text _text3;
+    [SerializeField] private Text _text;
     [SerializeField] private float _duration = 3F;
 
     private void Start()
     {
-        _text1.DOText("secret file.vrml", _duration);
-        _text2.DOText(" file not found Exception..", _duration).SetRelative();
-        _text3.DOText("the file could not be executed on the device, definitely...", _duration, true, ScrambleMode.All);
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(_text.DOText("secret file.vrml", _duration));
+        sequence.Append(_text.DOText(" file not found Exception..", _duration).SetRelative());
+        sequence.Append(_text.DOText("the file could not be executed on the device, definitely...", _duration, true, ScrambleMode.All));
     }
 }
